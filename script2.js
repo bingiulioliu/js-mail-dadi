9d5fc45873194353b65b33108cfef02b4cf76985
Bin Giulio Liu <bingiulio.liu@gmail.com>
2026-03-26 17:06:08 +0100

    DONE gioco dei dadi con array

--- script2.js
+++ script2.js
@@ -1,7 +1,35 @@
 /////////////////////////////////////////
 // Gioco dei dadi ////////////
 /////////////////////////////////////////
-let dadoPlayer = (Math.floor(Math.random() * 6 + 1));
-let dadoPc = (Math.floor(Math.random() * 6 + 1));
-console.log(dadoPlayer);
-console.log(dadoPc);
+
+// Creo due numeri random da 1 a 6, il primo assegnato al player e il secondo al pc
+// alert ('Gioco dei dadi! Clicca OK per giocare');
+// let dadoPlayer = (Math.floor(Math.random() * 6 + 1));
+// let dadoPc = (Math.floor(Math.random() * 6 + 1));
+
+// if  (dadoPlayer > dadoPc) {
+//     alert('Player tira: ' + dadoPlayer + ' PC tira: ' + dadoPc + ' Vince Player!');
+// }
+//     else if (dadoPlayer < dadoPc) {
+//         alert('Player tira: ' + dadoPlayer + ' C tira: ' + dadoPc + ' Vince PC!');
+// }   
+//     else {
+//         alert('Player tira: ' + dadoPlayer + ' PC tira: ' + dadoPc + ' Pareggio!');
+//     }
+
+// Gioco dei dadi con array
+const dSei = [1, 2, 3, 4, 5, 6];
+const dadoPlayer = (Math.floor(Math.random() * 6 + 1));
+const dadoPc = (Math.floor(Math.random() * 6 + 1));
+
+alert ('Gioco dei dadi! Clicca OK per giocare');
+
+if  (dSei[dadoPlayer] > dSei[dadoPc]) {
+    alert('Player tira: ' + dadoPlayer + ' PC tira: ' + dadoPc + ' Vince Player!');
+}
+    else if (dSei[dadoPlayer] < dSei[dadoPc]) {
+        alert('Player tira: ' + dadoPlayer + ' C tira: ' + dadoPc + ' Vince PC!');
+}   
+    else {
+        alert('Player tira: ' + dadoPlayer + ' PC tira: ' + dadoPc + ' Pareggio!');
+    }
